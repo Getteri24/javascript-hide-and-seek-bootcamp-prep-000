4eeedc41b09c778a0b6003f859bb206408d23271
t bryant <trbryant1981@gmail.com>
2019-01-03 05:59:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.getElementById('nested').querySelector(".target");
+}
+
+function increaseRankBy(n) {
+  let rank  = document.querySelectorAll(".ranked-list");
+  
+  for (let i=0, m=rank.length; i<m; i++) {
+    let children = rank[i].children;
+    
+    for(let j=0, k=children.length; j<k; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n;
+    }
+  }
+}
+
+function deepestChild() {
+  let divs = document.getElementById("grand-node").querySelectorAll("div");
+  let deepest;
+    for(let i = 0; i < divs.length -1; i++) {
+     deepest = divs[i].querySelector("div");
+  }
+return deepest;
+  
+}
